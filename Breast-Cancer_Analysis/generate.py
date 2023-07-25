556bcaaa351e609500866879b4da4872817a22b4
Avik Agarwala <98759608+AvikAgarwala@users.noreply.github.com>
2023-07-26 01:29:36 +0530

    Update generate.py
    Improve code

--- Breast-Cancer_Analysis/generate.py
+++ Breast-Cancer_Analysis/generate.py
@@ -2,14 +2,26 @@ import random
 import csv
 import numpy as np
 
-print("Generating data...")
-#generate data
-num_samples = random.randint(1,100)
-random_samples = np.random.rand(num_samples, 30) * 1000
-formatted_samples = [["{:.3e}".format(value) for value in sample] for sample in random_samples]
-file_generate = "random_samples.csv"
-with open("new_samples.csv", "w", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerows(formatted_samples)
+def generate_random_samples(num_samples, num_features):
+    random_samples = np.random.rand(num_samples, num_features) * 1000
+    return random_samples
 
-print(f"Generated data saved to '{file_generate}'.")
+def save_samples_to_csv(samples, filename):
+    with open(filename, "w", newline="") as file:
+        writer = csv.writer(file)
+        for sample in samples:
+            formatted_sample = ["{:.3e}".format(value) for value in sample]
+            writer.writerow(formatted_sample)
+
+def main():
+    print("Generating data...")
+    num_samples = random.randint(1, 100)
+    num_features = 30
+    random_samples = generate_random_samples(num_samples, num_features)
+    file_generate = "random_samples.csv"
+    save_samples_to_csv(random_samples, file_generate)
+
+    print(f"Generated data saved to '{file_generate}'.")
+
+if __name__ == "__main__":
+    main()
